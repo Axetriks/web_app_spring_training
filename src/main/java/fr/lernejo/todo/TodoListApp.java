bfaf2700b9ea36ef4900d8ea8208b19015954d5b
axetriks <apascal@et.esiea.fr>
2023-02-13 11:50:18 +0100

    HTTP endpoints API creation

---
+++ src/main/java/fr/lernejo/todo/TodoListApp.java
@@ -0,0 +1,15 @@
+package fr.lernejo.todo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TodoListApp {
+    public static void main(String[] args){
+        SpringApplication.run(TodoListApp.class, args);
+    }
+}
---
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -0,0 +1,37 @@
+package fr.lernejo.todo;
+
+import java.util.ArrayList;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerErrorException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+public class TodoListController {
+    
+    public final ArrayList<Todo> todos;
+
+    public TodoListController(ArrayList<Todo> todos){
+        this.todos = todos;
+    }
+    
+    @GetMapping("api/todo")
+    public ResponseEntity<ArrayList<Todo>> getTodo(){
+        return new ResponseEntity<>(this.todos, HttpStatus.OK);
+    }
+
+    @PostMapping(path = "api/todo",
+        consumes = MediaType.APPLICATION_JSON_VALUE, 
+        produces = MediaType.APPLICATION_JSON_VALUE)
+    public void addTodo(@RequestBody Todo newTodo){
+        
+        if(newTodo == null){
+            throw new ServerErrorException("Nothing to add");
+        }else this.todos.add(newTodo);    
+    }
+}
